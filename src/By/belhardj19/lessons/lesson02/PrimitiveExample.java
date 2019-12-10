54380fb8414e85342fc946cdafda657a3fa628c2
unknown <vinnichenkoe@tut.by>
2019-12-10 20:36:36 +0300

    init

---
+++ src/By/belhardj19/lessons/lesson02/PrimitiveExample.java
@@ -0,0 +1,19 @@
+package By.belhardj19.lessons.lesson02;
+
+public class PrimitiveExample {
+
+    //psvm
+    public static void main(String[] args) {
+        short sh = 5;
+        int i = 100;
+        double d = 3.14;
+        System.out.println((sh + d) / i);
+        //sout
+
+        int k = 0;
+        System.out.println(k++);
+        System.out.println(++k);
+
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        System.out.println("Im V");
+        System.out.println("lolgfre");
+        System.out.println("regfd");
+        System.out.println("ergdfg");
+
+    }
+
+
+}
