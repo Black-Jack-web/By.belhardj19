9e31f02688e40f76ae8c7707297981297357fa2f
Mishanya <gurin-100@mail.ru>
2019-12-11 21:13:55 +0300

    homework

--- src/By/belhardj19/lessons/lesson02/homework.java
+++ src/By/belhardj19/lessons/lesson02/homework.java
@@ -1,4 +1,30 @@
 package By.belhardj19.lessons.lesson02;
 
+
 public class homework {
+
+    public static void main(String[] args) {
+        byte a = 5;
+        short b = 123;
+        char c = 'G';
+        int d = 191;
+        long e = 100;
+        double k = 8.53;
+        boolean s = true;
+        System.out.println(a * 5 - 52);
+        System.out.println(b - 23 * 2);
+        System.out.println(c);
+        System.out.println(d + 22 / 2);
+        System.out.println(e * 20 % 34);
+        System.out.println(k + k / 2);
+        System.out.println("Is easy " + s);
+
+        int r=5;
+        System.out.println(3.14 * 25);
+        //Число П умноженное на 5 в квадрате
+    }
 }
+
+
+
+
